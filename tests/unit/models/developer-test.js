396fae2f0a073e539324473a64dff48e8cbfb128
Nadeem Khedr <nadeemkhedr@gmail.com>
2015-12-21 12:33:47 +0200

    Fix failing tests

--- tests/unit/models/developer-test.js
+++ tests/unit/models/developer-test.js
@@ -2,7 +2,7 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('developer', 'Unit | Model | developer', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:skill']
 });
 
 test('it exists', function(assert) {
