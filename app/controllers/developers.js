be3a76ceaa8408318dcf964f81b585d412c9e39b
Nadeem Khedr <nadeemkhedr@gmail.com>
2015-12-23 01:53:31 +0200

    #14 code cleanup

--- app/controllers/developers.js
+++ app/controllers/developers.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  selectedPage: 1,
+
   actions: {
     selectPage: function(page) {
-      this.transitionToRoute('developers',{queryParams: {page}});
+      this.transitionToRoute({queryParams: {page}});
     }
   }
 
