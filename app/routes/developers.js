264329290db5cf64ffbd0de2b1859ae4cbf4a636
Nadeem Khedr <nadeemkhedr@gmail.com>
2015-12-22 17:42:59 +0200

    #14 Finish integrating pagination with developers page using query string params, updated mock server to reflect the changes

--- app/routes/developers.js
+++ app/routes/developers.js
@@ -1,12 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  queryParams: {
+    page: {
+      refreshModel: true
+    }
+  },
+  model(params) {
+    var page = +params.page || 1;
     return Ember.RSVP.hash({
-      developers: this.store.findAll('developer')
+      developers: this.store.query('developer', {page}),
+      page
     });
   },
   setupController (controller, model) {
-    controller.set('developers', model.developers);
+    controller.setProperties({
+      developers: model.developers,
+      page: model.page,
+      totalItemsCount: model.developers.get('meta.totalItemsCount')
+    });
   }
 });
--- server/mocks/developers.js
+++ server/mocks/developers.js
@@ -4,29 +4,24 @@ module.exports = function(app) {
   var developersRouter = express.Router();
 
   developersRouter.get('/', function(req, res) {
+    var page = +req.query.page || 1,
+        result = getDevelopers(),
+        allDevelopersCount = result.length,
+        itemsPerPage = 10,
+        totalPages = Math.ceil(allDevelopersCount / itemsPerPage);
+
+    if(page > totalPages) {
+      return res.send(400);
+    }
+
+    var start = ((page - 1) * itemsPerPage) + 1;
+    var end = start + (itemsPerPage - 1);
+
+    result = result.slice(start - 1, end);
+
     res.send({
-      data: [1, 2, 3, 4, 5].map((id) => {
-        return {
-          "type": "developer",
-          "id": `${id}`,
-          "attributes": {
-            "available": true,
-            "availableDate": new Date(),
-            "name": "Kamil Ogórek",
-            "first-name": "Kamil",
-            "last-name": "Ogórek",
-            "created-at": new Date(),
-            "updated-at": new Date(),
-            "profile-url": "https://github.com/kamilogorek/",
-            "image-url": "https://avatars2.githubusercontent.com/u/1523305?v=3&s=460",
-            "address": "Kraków, PL",
-            "location": "39.5500507,-105.7820674",
-            "timezone": "Europe/Warsaw",
-            "rate": 128,
-            "skills": ["JavaScript", "Node.js"]
-          }
-        };
-      })
+      data: result,
+      meta: {totalItemsCount: allDevelopersCount}
     });
   });
 
@@ -65,4 +60,33 @@ module.exports = function(app) {
   //
   //app.use('/api/developers', require('body-parser'));
   app.use('/api/developers', developersRouter);
+};
+
+
+var getDevelopers = function() {
+  //create array from 1..95
+  var result = new Array(95).fill().map((x, index) => index + 1);
+
+  return result.map((id) => {
+    return {
+      "type": "developer",
+      "id": `${id}`,
+      "attributes": {
+        "available": true,
+        "availableDate": new Date(),
+        "name": `Kamil Ogórek ${id}`,
+        "first-name": `Kamil ${id}`,
+        "last-name": `Ogórek ${id}`,
+        "created-at": new Date(),
+        "updated-at": new Date(),
+        "profile-url": "https://github.com/kamilogorek/",
+        "image-url": "https://avatars2.githubusercontent.com/u/1523305?v=3&s=460",
+        "address": "Kraków, PL",
+        "location": "39.5500507,-105.7820674",
+        "timezone": "Europe/Warsaw",
+        "rate": 128,
+        "skills": ["JavaScript", "Node.js"]
+      }
+    };
+  })
 };
