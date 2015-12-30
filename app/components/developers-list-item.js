9f3bf9732e842186a05a00f9baae539ffbbc2db6
Nadeem Khedr <nadeemkhedr@gmail.com>
2015-12-30 20:45:58 +0200

    #19 Finish design for developers list, refacotring scss structure again

--- app/components/developers-list-item.js
+++ app/components/developers-list-item.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['list-group-item']
+  classNames: ['profile-list-item']
 });
--- app/components/developers-list.js
+++ app/components/developers-list.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['list-group']
+  classNames: ['list-two-columns'],
+  tagName: 'ul'
 });
