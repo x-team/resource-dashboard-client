e0da2119b7fa0a618ccf79014406c1030244ed30
Nadeem Khedr <nadeemkhedr@gmail.com>
2016-01-27 00:40:17 +0200

    Fix filtering bug when having null values

--- app/components/developers-list-filters.js
+++ app/components/developers-list-filters.js
@@ -6,19 +6,19 @@ export default Ember.Component.extend({
 
   allSkills: Ember.computed('developers.[]', function() {
     let allSkills = this.get('developers').mapBy('skills');
-    return _.chain(allSkills).flatten().sort().uniq(true).value();
+    return _.chain(allSkills).flatten().reject(x => !x).sort().uniq(true).value();
   }),
   allAddresses: Ember.computed('developers.[]', function() {
     let allAddresses = this.get('developers').mapBy('address');
-    return _.chain(allAddresses).sort().uniq(true).value();
+    return _.chain(allAddresses).reject(x => !x).sort().uniq(true).value();
   }),
   allTimezones: Ember.computed('developers.[]', function() {
     let allTimezones = this.get('developers').mapBy('timezone');
-    return _.chain(allTimezones).sort().uniq(true).value();
+    return _.chain(allTimezones).reject(x => !x).sort().uniq(true).value();
   }),
-  allRates: Ember.computed('developers.[]', function() {
+  allRates: Ember.computed('developers.@each.rate', function() {
     let allRates = this.get('developers').mapBy('rate');
-    return _.chain(allRates).sort().uniq(true).value();
+    return _.chain(allRates).reject(x => !x).sort().uniq(true).value();
   }),
   actions: {
     select(field, value) {
